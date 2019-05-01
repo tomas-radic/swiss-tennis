cdb7a0c2196a7fadb5dc993d05a251afa255f8ed
Tomas Radic <tomas.radic@gmail.com>
2019-05-01 17:27:36 +0200

    Round links added

--- app/helpers/rounds_helper.rb
+++ app/helpers/rounds_helper.rb
@@ -1,2 +1,11 @@
 module RoundsHelper
+  def round_element(round, user)
+    return nil unless round.present?
+
+    if user.present?
+      link_to round.full_label, round_path(round), class: 'btn btn-success'
+    else
+      round.full_label
+    end
+  end
 end
