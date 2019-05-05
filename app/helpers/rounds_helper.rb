c915a6274c9bbb174e7706e49f4964e24c3c2a65
Tomas Radic <tomas.radic@gmail.com>
2019-05-05 15:19:57 +0200

    Added match instructions, added date/time localizations

--- app/helpers/rounds_helper.rb
+++ app/helpers/rounds_helper.rb
@@ -13,13 +13,13 @@ module RoundsHelper
     return nil if round.period_begins.nil? && round.period_ends.nil?
 
     text = "Kolo #{round.position} sa hrá"
-    text += " od #{round.period_begins.to_formatted_s(:short)}" if round.period_begins.present?
-    text += " do #{round.period_ends.to_formatted_s(:short)}" if round.period_ends.present?
+    text += " od #{I18n.localize(round.period_begins, format: :date_month)}" if round.period_begins.present?
+    text += " do #{I18n.localize(round.period_ends, format: :date_month)}" if round.period_ends.present?
     text += '.'
 
     result_html = "<p><i><small>#{text}"
     if pending_matches?(round) && last_days?(round)
-      result_html += " <b><span class=\"text-danger\">Prosíme hráčov aby odohrali a nahlásili svoj zápas do termínu #{round.period_ends.to_formatted_s(:short)}.</span></b>"
+      result_html += " <b><span class=\"text-danger\">Prosíme hráčov aby odohrali a nahlásili výsledok svojho zápasu do termínu #{I18n.localize(round.period_ends, format: :date_month)}.</span></b>"
     end
     result_html += "</small></i></p>"
     result_html.html_safe
