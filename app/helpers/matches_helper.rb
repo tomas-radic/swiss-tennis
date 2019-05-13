8396b4c9f7d0a6f0d23fe95a440054051989c908
Tomas Radic <tomas.radic@gmail.com>
2019-05-13 23:53:58 +0200

    Finished matches colored

--- app/helpers/matches_helper.rb
+++ app/helpers/matches_helper.rb
@@ -6,4 +6,38 @@ module MatchesHelper
       '<span class="badge badge-pill badge-warning">Zápas nie je zverejnený.</span>'.html_safe
     end
   end
+
+  def match_table_row(match)
+    color_class = if match.finished_at.present?
+      if match.finished_at > Date.yesterday.midnight
+        'bg-yellow'
+      else
+        'bg-green'
+      end
+    else
+      ''
+    end
+
+    result_html = "<tr class=\"#{color_class}\"><td>"
+
+    if match.player1_id == match.winner_id
+      result_html += "<strong>#{match.player1.name}</strong>"
+    else
+      result_html += match.player1.name
+    end
+
+    result_html += "<br><small><i>#{link_to match.player1.phone, "tel:#{match.player1.phone}"}</i></small></td><td>"
+
+    if match.player2_id == match.winner_id
+      result_html += "<strong>#{match.player2.name}</strong>"
+    else
+      result_html += match.player2.name
+    end
+
+    result_html += "<br><small><i>#{link_to match.player2.phone, "tel:#{match.player2.phone}"}</i></small></td>"
+    result_html +=  "<td>#{match.note}</td>"
+    result_html += "<td>#{MatchDecorator.new(match).score}</td>"
+    result_html += "<td>#{link_to 'Detail', match_path(match), class: 'btn btn-sm btn-success'}</td></tr>"
+    result_html.html_safe
+  end
 end
