280e1522d521d383e1502af7e3420b5cb26de313
Tomas Radic <tomas.radic@gmail.com>
2019-06-13 13:53:24 +0200

    Added migration to set match loosers

--- db/migrate/20190611132403_add_looser_id_to_matches.rb
+++ db/migrate/20190611132403_add_looser_id_to_matches.rb
@@ -2,17 +2,4 @@ class AddLooserIdToMatches < ActiveRecord::Migration[5.2]
   def change
     add_column :matches, :looser_id, :uuid, foreign_key: true
   end
-
-  Match.all.each do |match|
-    if match.winner
-      looser = if match.winner == match.player1
-        match.player2
-      elsif match.winner == match.player2
-        match.player1
-      end
-
-      match.looser = looser
-      match.save!
-    end
-  end
 end
