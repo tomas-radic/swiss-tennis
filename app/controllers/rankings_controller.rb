c2c1e17d63498209b462f38f97abd5653c8776f1
Tomas Radic <tomas.radic@gmail.com>
2019-06-20 18:41:48 +0200

    Updating toss points

--- app/controllers/rankings_controller.rb
+++ app/controllers/rankings_controller.rb
@@ -1,13 +1,32 @@
 class RankingsController < ApplicationController
+  before_action :verify_user_logged_in, except: [:index]
   before_action :load_season, only: [:index]
   before_action :load_round, only: [:index]
+  before_action :load_record, only: [:edit, :update]
 
   def index
     @rankings = RankingsQuery.call(round: @round)
     @last_update_time = @rankings.pluck(:updated_at).max&.in_time_zone
   end
 
+  def edit
+
+  end
+
+  def update
+    @ranking.update!(permitted_params)
+    redirect_to rankings_path(round_id: @ranking.round_id)
+  end
+
   private
+
+  def permitted_params
+    params.require(:ranking).permit(:toss_points)
+  end
+
+  def load_record
+    @ranking = Ranking.find(params[:id])
+  end
 
   def load_season
     @season = if params[:season_id]
