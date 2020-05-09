cab85b30224b20884c8ac70f2836804dd36bf038
tomas-radic <tomas.radic@gmail.com>
2020-05-09 19:52:16 +0200

    Root page changed

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,8 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
-  # root to: 'matches#index'
-  root to: 'enrollments#index'
+  root to: 'matches#index'
+  # root to: 'enrollments#index'
 
   resources :users, only: [:edit, :update]
   resources :sessions, only: [:new, :create, :destroy]
