2b731a2c0acda4bf1b7285dcd7a481544ee4c8bb
tomas-radic <tomas.radic@gmail.com>
2020-06-02 11:28:55 +0200

    Changed sorting of matches

--- app/models/match.rb
+++ app/models/match.rb
@@ -28,7 +28,7 @@ class Match < ApplicationRecord
             :looser,
             presence: true, if: :finished?
 
-  scope :default, -> { order('matches.finished_at desc nulls last, matches.play_date asc nulls last, matches.updated_at desc, matches.note nulls last') }
+  scope :default, -> { order("matches.finished_at desc nulls last, matches.play_date asc nulls last").order(note: :asc, updated_at: :desc) }
   scope :manual, -> { where(from_toss: false) }
   scope :toss, -> { where(from_toss: true) }
   scope :published, -> { default.where(published: true) }
