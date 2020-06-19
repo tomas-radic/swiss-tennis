66a20928b943d07df46dd38ee79aee13b3ed23cf
tomas-radic <tomas.radic@gmail.com>
2020-06-19 22:37:21 +0200

    Method handicap2 - calculates points of opponents

--- app/models/ranking.rb
+++ app/models/ranking.rb
@@ -9,4 +9,17 @@ class Ranking < ApplicationRecord
             :games_difference,
             presence: true
 
+  def handicap2
+    opponents = Player.joins(matches: :round).merge(Match.finished)
+                    .where("rounds.position <= ?", self.round.position)
+                    .where("rounds.season_id = ?", self.round.season_id)
+                    .where("matches.player1_id = ? OR matches.player2_id = ?", self.player_id, self.player_id)
+                    .where.not(id: self.player_id)
+                    .where("matches.winner_id = ? or (matches.set1_player1_score is not null or matches.set1_player2_score is not null)", self.player_id)
+
+    Ranking.joins(:round)
+        .where(player_id: opponents.ids)
+        .where(rounds: { season_id: self.round.season_id })
+        .where("rounds.position = ?", self.round.position).inject(0) { |sum, ranking| sum += ranking.points }
+  end
 end
