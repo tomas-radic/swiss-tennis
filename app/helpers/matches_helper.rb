18c76a4eeae1f1b48573373fa47cdb7b5dfc7fcd
tomas-radic <tomas.radic@gmail.com>
2021-03-28 21:24:23 +0200

    Changed colors of matches table

--- app/helpers/matches_helper.rb
+++ app/helpers/matches_helper.rb
@@ -1,4 +1,8 @@
 module MatchesHelper
+
+  RECENT = Date.yesterday.midnight
+
+
   def match_published_pill(match)
     if match.finished?
       '<span class="badge badge-pill badge-success">Zápas je uzavretý.</span>'.html_safe
@@ -9,15 +13,34 @@ module MatchesHelper
     end
   end
 
-  def matches_table_row_color_class(match)
+
+  def match_css_class(match)
     if match.finished_at.nil?
       ''
-    elsif match.finished_at > Date.yesterday.midnight
-      'bg-yellow'
+    elsif match.finished_at > RECENT
+      'table-bg-yellow'
     else
-      'bg-green'
+      'table-bg-green'
     end
   end
+
+
+  def match_player_css_class(match, player)
+    if match.finished_at.nil?
+      ''
+
+    elsif player == match.winner
+      if match.finished_at > RECENT
+        'font-weight-bold table-bg-darken-yellow'
+      else
+        'font-weight-bold table-bg-darken-green'
+      end
+
+    elsif player == match.retired_player
+      'dark-gray'
+    end
+  end
+
 
   def match_info(match)
     play_date = I18n.l(match.finished_at&.to_date || match.play_date, format: :date_month) if match.finished_at || match.play_date
@@ -31,6 +54,7 @@ module MatchesHelper
     result
   end
 
+
   def delayed_matches_announcement(delayed_matches)
     count = delayed_matches.count
 
@@ -42,6 +66,7 @@ module MatchesHelper
       "... plus #{count} neodohratý zápas z predchádzajúcich kol"
     end
   end
+
 
   def unplanned_matches_info(matches_count)
     word = if matches_count > 4
