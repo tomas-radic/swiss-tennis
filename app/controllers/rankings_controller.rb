1a66d3d8c059fe01e8ed9352c7bf377016010143
dino <tomas.radic@gmail.com>
2021-06-23 14:23:26 +0200

    Rankings page - matches to play - ordered

--- app/controllers/rankings_controller.rb
+++ app/controllers/rankings_controller.rb
@@ -3,7 +3,10 @@ class RankingsController < ApplicationController
   def index
     if selected_round
       @rankings = SortedRankings.result_for(round: selected_round)
-      @matches_to_play = Match.published.pending.not_dummy.includes(:players)
+      @matches_to_play = Match.published.pending.not_dummy
+                              .joins(:round).where("rounds.position <= ?", selected_round.position)
+                              .includes(:players, :round)
+
       @last_update_time = @rankings.map { |r| r[:updated_at] }.max&.in_time_zone
     else
       @rankings = []
