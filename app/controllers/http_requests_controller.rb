378d65cacac88a96cee7aec30f5209ed49c89d19
dino <tomas.radic@gmail.com>
2021-07-17 17:49:01 +0200

    HttpRequests ordered

--- app/controllers/http_requests_controller.rb
+++ app/controllers/http_requests_controller.rb
@@ -6,6 +6,11 @@ class HttpRequestsController < ApplicationController
   def index
     @year = (params[:year] || Date.today.year).to_i
     @http_requests = HttpRequest.where(year: @year)
+                                .order(
+                                  week: :desc,
+                                  count: :desc,
+                                  ip_address: :asc,
+                                  path: :asc)
   end
 
 end
