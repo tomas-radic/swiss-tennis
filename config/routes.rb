5fde454f07b8d3986fd4dfbf7c77793e1e468031
dino <tomas.radic@gmail.com>
2022-05-01 16:42:17 +0200

    Changed root path.

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,8 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
-  # root to: 'matches#index'
-  root to: "pages#season2022"
+  root to: 'matches#index'
+  # root to: "pages#season2022"
 
   resources :users, only: [:edit, :update]
   resources :sessions, only: [:new, :create, :destroy]
